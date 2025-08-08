79d6cf56a19fdd8312f58df64088841e21860291
Harley Green <harleyrobertgreen@hotmail.com>
2025-08-08 13:59:48 +0100

    Changed sent email markdown layout

--- resources/views/mail/lead-failed.blade.php
+++ resources/views/mail/lead-failed.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body style="font-family: sans-serif; line-height:1.4; color:#444;">
     <h2 style="color:#222">Resubmit Your Details</h2>
-    <p>Hello{{ $lead->name ? ' ' . e($lead->name) : '' }},</p>
+    <p>Hello{{ $lead->first_name ? ' ' . e($lead->first_name) : '' }},</p>
     <p>We weren’t able to process your submission. To complete your application, click the button below:</p>
     <p>
         <a href="{{ config('app.frontend_url'). '/form/' . $lead->id }}" style="display:inline-block;padding:10px 20px;background:#2d3748;color:#fff;text-decoration:none;border-radius:4px;">
@@ -15,6 +15,6 @@
     </p>
     <p>If that doesn’t work, copy &amp; paste:</p>
     <p style="word-break:break-all;"><a href="{{ config('app.frontend_url'). '/form/' . $lead->id}}">{{ config('app.frontend_url'). '/form/' . $lead->id }}</a></p>
-    <p>Thanks,<br>The {{ config('app.name') }} Team</p>
+    <p>Thanks,<br>- H</p>
 </body>
 </html>
